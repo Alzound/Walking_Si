6c733d91fc34af5e79ad3a9e2d3b31e48043c660
Alzound <aaxlcd@gmil.com>
2020-05-30 23:04:39 -0500

    #1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offSet;
+    public float rotateSpeed;
+    public bool useOffSetValues;
+    public Transform pivot;
+
+    private void Start()
+    {
+        if (!useOffSetValues)
+        {
+            offSet = target.position - transform.position;
+        }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //This makes our cursor to stop being visble when we start playing the game 
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    private void Update()
+    {
+        //All of this makes that the camera can't go beyond a certain limit, in the case of looking up or lookind down, or at least it's supposed to do.
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, -horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);
+
+        if (pivot.rotation.eulerAngles.x > 45 && pivot.rotation.eulerAngles.x < 180)
+        {
+            pivot.rotation = Quaternion.Euler(65f, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 315)
+        {
+            pivot.rotation = Quaternion.Euler(315f, 0, 0);
+        }
+
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offSet);
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+        transform.LookAt(target);
+
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+
+    [Header("Character options")]
+    public float gravity = 20.0f; 
+    public float movementSpeed = 80;
+    public float jumpSpeed = 8.0f;
+
+    [Header("Camera options")]
+    public Camera cam;
+    public float mouseH = 3.0f;
+    public float mouseV = 2.0f;
+    public float minRotation = -65.0f;
+    public float maxRotation = 60.0f;
+    float h_Mouse, v_Mouse;
+
+    private Vector3 move = Vector3.zero;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        h_Mouse = mouseH * Input.GetAxis("Mouse X");
+        v_Mouse += mouseV * Input.GetAxis("Mouse Y");
+
+        v_Mouse = Mathf.Clamp(v_Mouse, minRotation, maxRotation);
+        cam.transform.localEulerAngles = new Vector3(-v_Mouse, 0, 0);
+
+        transform.Rotate(0, h_Mouse, 0);
+
+
+        Movement();
+   
+    }
+
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            move = transform.TransformDirection(move) * movementSpeed; 
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                move.y = jumpSpeed;
+            }
+        }
+        move.y -= gravity * Time.deltaTime;
+
+        controller.Move(move * Time.deltaTime);
+    }
+}
